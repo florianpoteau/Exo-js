cffbed4c7ee89657ddccc13a29eff6889b90a544
Poteau Florian <florianpoteau59@gmail.com>
2023-05-16 11:09:44 +0200

    ajout: exercice 2

---
+++ exo 2/script2.js
@@ -0,0 +1,21 @@
+const img = document.querySelector(".img")
+const img1 = document.querySelector(".img1")
+let p1 = document.querySelector(".p1")
+const rep = document.querySelector(".rep")
+
+function image (img, image){
+
+img.addEventListener("click", () =>{
+    img.style.transform = "scale(150%)"
+    p1.textContent = `L'image que vous avez séléctionné est: ${image}`
+    img.style.border = "2px solid red"
+})
+
+
+
+
+
+}
+
+image(img, img.alt)
+image(img1, img1.alt)
